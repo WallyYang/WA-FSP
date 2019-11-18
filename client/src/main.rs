20256d8942462cb14eff900620ae6b706a3dc564
Wally Yang <wallyyang28@gmail.com>
2019-11-18 09:50:49 -0500

    feat: added read server config

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,13 +1,45 @@
-use std::net::UdpSocket;
+use std::collections::HashMap;
+use std::fs::{self, DirEntry, File};
+use std::io::Read;
+use std::net::{SocketAddr, UdpSocket};
+use std::path::Path;
 use std::{io, str};
 
+use client::Server;
+
+struct Session {
+    server: Server,
+    files: HashMap<String, fs::File>,
+}
+
 fn main() {
+    // create session with server config and files
+    let mut session = Session {
+        server: Server::from_file("config.yaml"),
+        files: HashMap::new(),
+    };
+    for entry in fs::read_dir(Path::new("./files/")).unwrap() {
+        let entry = entry.unwrap();
+        session.files.insert(
+            entry.file_name().into_string().unwrap(),
+            fs::File::open(entry.path()).unwrap(),
+        );
+    }
+
+    // create UDP socket and connect to server
     let socket = UdpSocket::bind("127.0.0.1:8000")
         .expect("Could not bind client socket");
-
     socket
-        .connect("localhost:8080")
+        .connect(SocketAddr::new(session.server.address, session.server.port))
         .expect("Could not connect to server");
+
+    for mut file in session.files {
+        println!("{}", file.0);
+        let mut content = String::new();
+        file.1.read_to_string(&mut content).unwrap();
+        println!("{}", content);
+    }
+
     loop {
         let mut input = String::new();
         let mut buffer = [0u8; 1500];
