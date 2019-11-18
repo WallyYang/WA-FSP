ffb3c4a992ad34e22a29c6d7f64443a00d89d469
Wally Yang <wallyyang28@gmail.com>
2019-11-18 11:32:54 -0500

    refactor: moved main into app struct

--- client/src/main.rs
+++ client/src/main.rs
@@ -7,55 +7,66 @@ use std::{io, str};
 
 use client::Server;
 
-struct Session {
-    server: Server,
+struct FspClient {
+    socket: UdpSocket,
     files: HashMap<String, fs::File>,
 }
 
-fn main() {
-    // create session with server config and files
-    let mut session = Session {
-        server: Server::from_file("config.yaml"),
-        files: HashMap::new(),
-    };
-    for entry in fs::read_dir(Path::new("./files/")).unwrap() {
-        let entry = entry.unwrap();
-        session.files.insert(
-            entry.file_name().into_string().unwrap(),
-            fs::File::open(entry.path()).unwrap(),
-        );
+impl FspClient {
+    fn new() -> FspClient {
+        let mut files = HashMap::new();
+        for entry in fs::read_dir(Path::new("./files/")).unwrap() {
+            let entry = entry.unwrap();
+            files.insert(
+                entry.file_name().into_string().unwrap(),
+                fs::File::open(entry.path()).unwrap(),
+            );
+        }
+
+        let server = Server::from_file("config.yaml");
+        let socket = UdpSocket::bind("127.0.0.1:8000")
+            .expect("Could not bind client socket");
+        socket
+            .connect(SocketAddr::new(server.address, server.port))
+            .expect("Could not connect to server");
+
+        FspClient { socket, files }
     }
+
+    fn run(&mut self) {
+        for mut file in &self.files {
+            println!("{}", file.0);
+            let mut content = String::new();
+            file.1.read_to_string(&mut content).unwrap();
+            println!("{}", content);
+        }
+
+        loop {
+            let mut input = String::new();
+            let mut buffer = [0u8; 1500];
+            io::stdin()
+                .read_line(&mut input)
+                .expect("Failed to read from stdin");
+            self.socket
+                .send(input.as_bytes())
+                .expect("Failed to write to server");
+
+            self.socket
+                .recv_from(&mut buffer)
+                .expect("Could not read into buffer");
+            print!(
+                "{}",
+                str::from_utf8(&buffer)
+                    .expect("Could not write buffer as string")
+            );
+        }
+    }
+}
+
+fn main() {
+    // create client with server config and files
+    let mut client = FspClient::new();
 
     // create UDP socket and connect to server
-    let socket = UdpSocket::bind("127.0.0.1:8000")
-        .expect("Could not bind client socket");
-    socket
-        .connect(SocketAddr::new(session.server.address, session.server.port))
-        .expect("Could not connect to server");
-
-    for mut file in session.files {
-        println!("{}", file.0);
-        let mut content = String::new();
-        file.1.read_to_string(&mut content).unwrap();
-        println!("{}", content);
-    }
-
-    loop {
-        let mut input = String::new();
-        let mut buffer = [0u8; 1500];
-        io::stdin()
-            .read_line(&mut input)
-            .expect("Failed to read from stdin");
-        socket
-            .send(input.as_bytes())
-            .expect("Failed to write to server");
-
-        socket
-            .recv_from(&mut buffer)
-            .expect("Could not read into buffer");
-        print!(
-            "{}",
-            str::from_utf8(&buffer).expect("Could not write buffer as string")
-        );
-    }
+    client.run();
 }
